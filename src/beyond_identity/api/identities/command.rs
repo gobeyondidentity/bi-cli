42386d91513c81e4eb7a4c29277b6884aec8c8a0
Ismail Mustafa <ismail.mustafa@zeropw.com>
2024-10-23 17:07:13 -0400

    Simplify command

--- src/beyond_identity/api/identities/command.rs
+++ src/beyond_identity/api/identities/command.rs
@@ -1,154 +1,93 @@
-use super::types::{Identities, IdentityFilterField, PatchIdentityDetails, PatchIdentityRequest};
+use super::types::{IdentityFilterField, PatchIdentityDetails, PatchIdentityRequest};
 use super::{
     api::{IdentitiesApi, IdentityService},
-    types::{CreateIdentityRequest, Identity, IdentityDetails, IdentityRequest},
+    types::{CreateIdentityRequest, IdentityDetails, IdentityRequest},
 };
 use crate::beyond_identity::api::common::filter::Filter;
-use crate::beyond_identity::api::groups::types::Groups;
-use crate::beyond_identity::api::roles::types::Roles;
 use crate::{beyond_identity::api::common::command::serialize, common::error::BiError};
-use clap::{Args, Subcommand};
+use clap::Subcommand;
 use std::str::FromStr;
 
-#[derive(Subcommand)]
+#[derive(Subcommand, Debug, Clone)]
 pub enum IdentityCommands {
-    Create(Create),
-    List(List),
-    Get(Get),
-    Patch(Patch),
-    Delete(Delete),
-    ListGroups(ListGroups),
-    ListRoles(ListRoles),
+    Create {
+        #[clap(flatten)]
+        identity_details: IdentityDetails,
+    },
+    List {
+        #[clap(long)]
+        filter: Option<String>,
+    },
+    Get {
+        #[clap(long)]
+        id: String,
+    },
+    Patch {
+        #[clap(long)]
+        id: String,
+        #[clap(flatten)]
+        identity_details: PatchIdentityDetails,
+    },
+    Delete {
+        #[clap(long)]
+        id: String,
+    },
+    ListGroups {
+        #[clap(long)]
+        id: String,
+    },
+    ListRoles {
+        #[clap(long)]
+        id: String,
+        #[clap(long)]
+        resource_server_id: String,
+    },
 }
 
 impl IdentityCommands {
     pub async fn execute(&self, service: &IdentityService) -> Result<String, BiError> {
         match self {
-            IdentityCommands::Create(cmd) => serialize(cmd.execute(service)).await,
-            IdentityCommands::List(cmd) => serialize(cmd.execute(service)).await,
-            IdentityCommands::Get(cmd) => serialize(cmd.clone().execute(service)).await,
-            IdentityCommands::Patch(cmd) => serialize(cmd.execute(service)).await,
-            IdentityCommands::Delete(cmd) => serialize(cmd.execute(service)).await,
-            IdentityCommands::ListGroups(cmd) => serialize(cmd.execute(service)).await,
-            IdentityCommands::ListRoles(cmd) => serialize(cmd.execute(service)).await,
-        }
-    }
-}
-
-#[derive(Args, Debug, Clone)]
-pub struct Create {
-    #[clap(flatten)]
-    pub identity_details: IdentityDetails,
-}
-
-impl Create {
-    pub async fn execute(&self, service: &IdentityService) -> Result<Identity, BiError> {
-        service
-            .create_identity(CreateIdentityRequest {
-                identity: IdentityRequest {
-                    display_name: self.identity_details.display_name.clone(),
-                    traits: self.identity_details.traits.clone(),
-                },
-            })
-            .await
-    }
-}
-
-#[derive(Args, Debug, Clone)]
-pub struct Delete {
-    /// The ID of the identity to retrieve
-    #[clap(long)]
-    pub id: String,
-}
-
-impl Delete {
-    pub async fn execute(&self, service: &IdentityService) -> Result<serde_json::Value, BiError> {
-        service.delete_identity(&self.id).await
-    }
-}
-
-#[derive(Args, Debug, Clone)]
-pub struct Get {
-    /// The ID of the identity to retrieve
-    #[clap(long)]
-    pub id: String,
-}
-
-impl Get {
-    pub async fn execute(&self, service: &IdentityService) -> Result<Identity, BiError> {
-        service.get_identity(&self.id).await
-    }
-}
-
-#[derive(Args, Debug, Clone)]
-pub struct List {
-    #[clap(long)]
-    pub filter: Option<String>,
-}
-
-impl List {
-    pub async fn execute(&self, service: &IdentityService) -> Result<Identities, BiError> {
-        service
-            .list_identities(Filter::new(
-                self.filter.clone(),
-                IdentityFilterField::from_str,
-            )?)
-            .await
-    }
-}
-
-#[derive(Args, Debug, Clone)]
-pub struct ListGroups {
-    /// The ID of the identity to list groups for
-    #[clap(long)]
-    pub id: String,
-}
-
-impl ListGroups {
-    pub async fn execute(&self, service: &IdentityService) -> Result<Groups, BiError> {
-        service.list_groups(&self.id).await
-    }
-}
-
-#[derive(Args, Debug, Clone)]
-pub struct ListRoles {
-    /// The ID of the identity to list roles for
-    #[clap(long)]
-    pub id: String,
-    /// The ID of the resource server used to filter roles
-    #[clap(long)]
-    pub resource_server_id: String,
-}
-
-impl ListRoles {
-    pub async fn execute(&self, service: &IdentityService) -> Result<Roles, BiError> {
-        service.list_roles(&self.id, &self.resource_server_id).await
-    }
-}
-
-#[derive(Args, Debug, Clone)]
-pub struct Patch {
-    /// The ID of the identity to patch
-    #[clap(long)]
-    pub id: String,
-
-    #[clap(flatten)]
-    pub identity_details: PatchIdentityDetails,
-}
-
-impl Patch {
-    pub async fn execute(&self, service: &IdentityService) -> Result<Identity, BiError> {
-        service
-            .patch_identity(
-                &self.id,
-                &PatchIdentityRequest {
-                    identity: PatchIdentityDetails {
-                        display_name: self.identity_details.display_name.clone(),
-                        status: self.identity_details.status.clone(),
-                        traits: self.identity_details.traits.clone(),
+            IdentityCommands::Create { identity_details } => {
+                serialize(service.create_identity(CreateIdentityRequest {
+                    identity: IdentityRequest {
+                        display_name: identity_details.display_name.clone(),
+                        traits: identity_details.traits.clone(),
                     },
-                },
-            )
-            .await
+                }))
+                .await
+            }
+            IdentityCommands::List { filter } => {
+                serialize(
+                    service.list_identities(Filter::new(
+                        filter.clone(),
+                        IdentityFilterField::from_str,
+                    )?),
+                )
+                .await
+            }
+            IdentityCommands::Get { id } => serialize(service.get_identity(id)).await,
+            IdentityCommands::Patch {
+                id,
+                identity_details,
+            } => {
+                serialize(service.patch_identity(
+                    id,
+                    &PatchIdentityRequest {
+                        identity: PatchIdentityDetails {
+                            display_name: identity_details.display_name.clone(),
+                            status: identity_details.status.clone(),
+                            traits: identity_details.traits.clone(),
+                        },
+                    },
+                ))
+                .await
+            }
+            IdentityCommands::Delete { id } => serialize(service.delete_identity(id)).await,
+            IdentityCommands::ListGroups { id } => serialize(service.list_groups(id)).await,
+            IdentityCommands::ListRoles {
+                id,
+                resource_server_id,
+            } => serialize(service.list_roles(id, resource_server_id)).await,
+        }
     }
 }
